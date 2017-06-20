ac4b86488ce55d06f2185a9172c2428a4bd403a9
junreturn1982 <ngochoang071982@gmail.com>
2017-06-20 14:27:35 +0700

    grammar

---
+++ 01helloWorldTypeScript/Scripts/02grammar.ts
@@ -0,0 +1,15 @@
+let x = 1;
+let y;
+let firstName = 'John';
+let lastName: string = 'Papa';
+
+let num1 = 100;
+let num2: number = 20;
+
+function addNumbers(n1: number, n2: number, n3: number) {
+    let result = n1 + n2 + n3;
+    let msg = 'Sum is = ' + result;
+    alert(msg);
+}
+
+addNumbers(num1, num2, 7);
